885b6bcc69477d24cf30ca173d997159abbc03b7
CJC-ds <carl.jason.chang@gmail.com>
2020-09-15 18:25:44 +1200

    added docstrings

--- src/get_replies.py
+++ src/get_replies.py
@@ -24,6 +24,17 @@ tweet_fields_of_interest = [
 ]
 
 def setup_api_config(path_to_credentials):
+    """
+    Handles setting up credentials to access the Twitter API via tweepy wrapper.
+
+    Parameters:
+    path_to_credentials (String):
+        Path to the credentials.ini file location.
+    Returns:
+    api (tweepy.api.API):
+        Twitter API wrapper object.
+        See http://docs.tweepy.org/en/latest/api.html
+    """
     # Configure the credentials
     CONFIG = configparser.ConfigParser()
     CONFIG.read(path_to_credentials)
@@ -44,6 +55,23 @@ def setup_api_config(path_to_credentials):
     return api
 
 def get_replies(api, twitter_user, tweet_id):
+    """
+    Uses the tweepy api and searches for specified twitter user and associated
+    tweet_id.
+
+    Parameters:
+    api (tweepy.api.API):
+        Twitter API wrapper object.
+        See http://docs.tweepy.org/en/latest/api.html
+    twitter_user (String):
+        Twitter user that owns the tweet we are interested in retrieving.
+    tweet_id (String):
+        Tweet id of tweet we are interested in retrieving.
+    Returns:
+    replies (list):
+        A list of Tweepy.models.status, or tweet status objects that
+        are in_reply_to specified tweet.
+    """
     replies = [
             tweet
             for tweet in tweepy.Cursor(
@@ -58,6 +86,20 @@ def get_replies(api, twitter_user, tweet_id):
     return replies
 
 def get_field(replies, tweet_field):
+    """
+    Parses the raw data retrieved from the Twitter API by flattening
+    and constructing a pandas.Series object specified by tweet_field.
+
+    Parameters:
+    replies (list):
+        Passing a list of replies of type Tweepy.models.status obtained
+        through iterating on the Tweepy cursor.
+    tweet_field (String):
+        Specifies hte tweet attribute/field within the json file to find.
+    Returns:
+    field_data_series (pandas.Series):
+        A pandas Series object containing the data of specified tweet_field.
+    """
     field_data = [flatten(reply._json)[tweet_field] for reply in replies]
     field_data_series = pd.Series(
         field_data,
@@ -66,6 +108,18 @@ def get_field(replies, tweet_field):
     return field_data_series
 
 def parse_tweet_url(tweet_url):
+    """
+    Splits the twitter url to tweet into two components.
+
+    Parameters:
+    tweet_url (String):
+        A url link to specified tweet.
+    Returns:
+    twitter_user (String):
+        The '@user' owner of the tweet.
+    tweet_id (String):
+        The tweet status id of interest.
+    """
     try:
         if ('twitter' not in tweet_url) & ('status' not in tweet_url):
             raise ValueError('Invalid twitter link.')
@@ -80,13 +134,13 @@ def parse_tweet_url(tweet_url):
         quit()
 
 def main():
+    # Setup path to credentials file.
     path = 'credentials.ini'
     try:
         print('Loading credentials')
         api = setup_api_config(path)
     except Exception as e:
         print('Invalid path to credentials.ini. credentials.ini file not found.')
-
     tweet_url = input('Tweet url: ')
     print('Parsing link...')
     twitter_user, tweet_id = parse_tweet_url(tweet_url)
@@ -94,6 +148,7 @@ def main():
     global tweet_fields_of_interest
     try:
         print('Retrieving tweets from server...')
+        # Retrieve replies from Twitter API
         replies = get_replies(
             api=api,
             twitter_user=twitter_user,
@@ -106,7 +161,7 @@ def main():
             for field in tweet_fields_of_interest
         ]
         # Combine the columns
-        df = pd.concat(s_list,axis=1)
+        df = pd.concat(s_list, axis=1)
 
         # Save file to disk.
         print('Saving...')
