37a1cc5e3b8c05c38be56b81ed27727b64f1acc3
CJC-ds <carl.jason.chang@gmail.com>
2020-10-18 12:09:24 +1300

    removed method to parse language codes to prerpocessing.py

--- src/to_gbq.py
+++ src/to_gbq.py
@@ -5,11 +5,13 @@ import itertools as it
 import re
 from google.oauth2 import service_account
 import pandas_gbq
-import requests
-import lxml.html as lh
+import configparser
 
-project_id = 'learn-291521'
-table_name = 'twitter.tweets'
+path_to_table_info = 'gbq_table_path.ini'
+CONFIG = configparser.ConfigParser()
+CONFIG.read(path_to_table_info)
+project_id = CONFIG['GBQ_PATH']['project_id']
+table_name = CONFIG['GBQ_PATH']['table_name']
 credentials = service_account.Credentials.from_service_account_file(
     'gbq_key.json')
 
@@ -26,28 +28,6 @@ def make_tweet_id_df(id, df):
     new_df.drop('index', axis=1, inplace=True)
     return new_df
 
-def add_full_lang_col(df):
-    # Scrape from the language code table at the url below
-    language_code_url = 'https://www.sitepoint.com/iso-2-letter-language-codes/'
-    r = requests.get(language_code_url)
-    doc = lh.fromstring(r.content)
-    # tr elements hold table rows. We can locate them via the xpath.
-    tr_elements = doc.xpath('//tr')
-    # Create a list of all the text content for each table row element.
-    content = [t.text_content() for t in tr_elements]
-    # Column names are found at content[0], only interested in content (starting at element 1)
-    # Remove the '\n' padding on the left and right of the string
-    # Split the string on the '\n' delimiter
-    parsed_content = [c.lstrip('\n').rstrip('\n').split('\n') for c in content[1:]]
-    # Unpack the parsed content
-    k, v = zip(*parsed_content)
-    # Lower case the code string to match the code in our twitter data.
-    v = [val.lower() for val in v]
-    language_dict = {val:key for key, val in zip(k,v)}
-    language_dict['und'] = 'Undetermined'
-    df['lang'] = df['lang'].replace(language_dict, regex=True)
-    return df
-
 def upload_all_archived():
     path = '../data/processed/'
     files = '*.pickle'
@@ -57,8 +37,6 @@ def upload_all_archived():
     tweet_ids = [int(s) for f in filenames for s in re.split(r'; |, |\\|\.', f) if s.isdigit()]
     new_dfs = [make_tweet_id_df(id, df) for df, id in zip(dfs, tweet_ids)]
     new_df = pd.concat(new_dfs)
-    new_df = add_full_lang_col(new_df)
-
     pandas_gbq.to_gbq(
         new_df,
         table_name,
@@ -71,8 +49,6 @@ def upload_specified_dataset(tweet_id):
     file_path = path + tweet_id +'.pickle'
     df = pd.read_pickle(file_path)
     new_df = make_tweet_id_df(int(tweet_id), df)
-    new_df = add_full_lang_col(new_df)
-
     pandas_gbq.to_gbq(
         new_df,
         table_name,
